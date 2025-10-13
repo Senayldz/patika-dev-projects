114b771430030ffdf9f97be63ed3aa7957e1b280
Senayldz <senasenayildiz@gmail.com>
2025-10-13 22:46:04 +0300

    Update ödev1.sql

--- SQL/Ödev1/ödev1.sql
+++ SQL/Ödev1/ödev1.sql
@@ -10,6 +10,7 @@ SELECT * FROM film
 WHERE rental_rate = 0.99 AND (replacement_cost = 12.99 OR replacement_cost = 28.99);
 
 --4.--
+--Cevap: Smith--
 SELECT * FROM customer
 WHERE first_name = 'Mary';
 
